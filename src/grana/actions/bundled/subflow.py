5ec3744f681bd94054182980cd0cceef584e4c4b
Artem Novikov <reartnew@nebius.com>
2025-01-02 23:36:34 +0100

    Context with deep update

--- src/grana/actions/bundled/subflow.py
+++ src/grana/actions/bundled/subflow.py
@@ -2,6 +2,8 @@
 
 import functools
 import typing as t
+from collections.abc import Mapping, MutableMapping
+from dataclasses import field
 from pathlib import Path
 
 from grana import ArgsBase, ActionBase, exceptions
@@ -17,6 +19,7 @@ class SubflowArgs(ArgsBase):
     """Arguments applied to the subflow action."""
 
     path: Path
+    context: t.Dict[str, t.Any] = field(default_factory=dict)  # pylint: disable=invalid-field-call
 
 
 class CompositeSource:
@@ -67,7 +70,27 @@ class SubflowAction(ActionBase):
                     event: DisplayEvent = await self._events_flow.get()
                     _resend_event_via_action(event)
 
+            @classmethod
+            def _deep_update(cls, receiver: t.Dict[str, t.Any], source: Mapping, path: str) -> t.Dict[str, t.Any]:
+                for source_key, source_value in source.items():
+                    sub_path: str = f"{path}.{source_key}" if path else source_key
+                    if source_key not in receiver:
+                        cls.logger.debug(f"Adding context: {sub_path}")
+                        receiver[source_key] = source_value
+                    elif isinstance(source_value, Mapping) and isinstance(receiver[source_key], MutableMapping):
+                        cls.logger.trace(f"Merging context: {sub_path}")
+                        receiver[source_key] = cls._deep_update(receiver[source_key], source_value, sub_path)
+                    else:
+                        cls.logger.debug(f"Rewriting context: {sub_path}")
+                        receiver[source_key] = source_value
+                return receiver
+
+            def update_context(self, source: Mapping) -> None:
+                """Apply changes to the context"""
+                self.workflow.context = self._deep_update(receiver=self.workflow.context, source=source, path="")
+
         runner = SubflowRunner(source=self.args.path)
+        runner.update_context(self.args.context)
         try:
             await runner.run_async()
         except exceptions.ExecutionFailed:
