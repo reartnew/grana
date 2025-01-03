59da3b6f080ca088bf29f88f3e622e05bffbce10
Artem Novikov <artnew@list.ru>
2025-01-03 18:56:04 +0100

    Feature/named message source attributes (#11)
    * Use renamed message source broadly
    
    * Check all event types
    
    ---------
    
    Co-authored-by: Artem Novikov <reartnew@nebius.com>

--- src/grana/actions/bundled/subflow.py
+++ src/grana/actions/bundled/subflow.py
@@ -7,7 +7,10 @@ from dataclasses import field
 from pathlib import Path
 
 from ..base import ArgsBase, ActionBase
-from ..types import NamedMessageSource, ActionStatus
+from ..types import (
+    NamedMessageSource,
+    RenamedMessageSource,
+)
 from ...display.types import DisplayEvent, DisplayEventName
 from ...exceptions import ExecutionFailed
 from ...rendering.containers import OutcomeDict
@@ -23,24 +26,7 @@ class SubflowArgs(ArgsBase):
     """Arguments applied to the subflow action."""
 
     path: Path
-    context: ContextType = field(default_factory=dict)  # pylint: disable=invalid-field-call
-
-
-class CompositeSource:
-    """Event source built from multiple sources"""
-
-    def __init__(self, *sources: NamedMessageSource) -> None:
-        self._sources: t.Tuple[NamedMessageSource, ...] = sources
-
-    @functools.cached_property
-    def name(self) -> str:
-        """Join component names"""
-        return "/".join(source.name for source in self._sources)
-
-    @property
-    def status(self) -> ActionStatus:
-        """Composite source status is the status of the first origin"""
-        return self._sources[-1].status
+    context: t.Dict[str, t.Any] = field(default_factory=dict)  # pylint: disable=invalid-field-call
 
 
 class SubflowAction(ActionBase):
@@ -54,20 +40,30 @@ class SubflowAction(ActionBase):
         action: SubflowAction = self
 
         @functools.lru_cache()
-        def _compose_source(origin: NamedMessageSource) -> CompositeSource:
-            return CompositeSource(self, origin)
+        def _compose_source(origin: NamedMessageSource) -> NamedMessageSource:
+            return RenamedMessageSource(name=f"{self.name}/{origin.name}", origin=origin)
 
         def _resend_event_via_action(event: DisplayEvent) -> None:
-            if event.name == DisplayEventName.ON_RUNNER_FINISH:
-                # This event shall not pass to the parent runner since it triggers final status output.
-                # Unlock the execution and continue.
-                event.future.set_result(None)
-                return
-            if event.name == DisplayEventName.ON_RUNNER_START:
+            # These events shall not pass to the parent runner
+            if event.name in (
+                DisplayEventName.ON_RUNNER_FINISH,  # Triggers final status output
+                DisplayEventName.ON_PLAN_INTERACTION,  # Pauses the execution
+            ):
+                event.future.set_result(None)  # Unlock the execution and continue
+            elif event.name == DisplayEventName.ON_RUNNER_START:
                 event.kwargs["children"] = map(_compose_source, event.kwargs["children"])
-            elif origin := event.kwargs.get("source"):
-                event.kwargs["source"] = _compose_source(origin)
-            self._event_queue.put_nowait(event)
+                self._event_queue.put_nowait(event)  # Pass modified event
+            elif event.name in (
+                DisplayEventName.ON_ACTION_START,
+                DisplayEventName.ON_ACTION_FINISH,
+                DisplayEventName.ON_ACTION_MESSAGE,
+                DisplayEventName.ON_ACTION_ERROR,
+            ):
+                event.kwargs["source"] = _compose_source(event.kwargs["source"])
+                self._event_queue.put_nowait(event)  # Pass modified event
+            else:
+                # Just in case we add some event types later and not specify behaviour here
+                raise ValueError(f"Unknown event name: {event.name!r}")  # pragma: no cover
 
         class SubflowRunner(Runner):
             """A runner that intercepts and filters out events"""
