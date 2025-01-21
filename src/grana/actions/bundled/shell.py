fcb4a627a2260f711e76c21443984c5de08c02e9
Artem Novikov <reartnew@nebius.com>
2025-01-21 16:07:20 +0100

    Get rid of async_shell

--- src/grana/actions/bundled/shell.py
+++ src/grana/actions/bundled/shell.py
@@ -1,10 +1,12 @@
 """Separate module for shell-related action"""
 
 import asyncio
+import contextlib
 import os
 import typing as t
-
-from async_shell import Shell, ShellResult
+from asyncio.streams import StreamReader
+from asyncio.subprocess import create_subprocess_shell, Process  # noqa
+from subprocess import PIPE
 
 from ..base import ArgsBase, EmissionScannerActionBase
 from ..types import Stderr
@@ -35,17 +37,30 @@ class ShellArgs(ArgsBase):
 class ShellAction(EmissionScannerActionBase):
     """Shell commands handler"""
 
+    _BYTES_LINE_SEPARATOR: bytes = os.linesep.encode()
+    _ENCODING: str = "utf-8"
     args: ShellArgs
 
-    async def _read_stdout(self, shell_process: Shell) -> None:
-        async for line in shell_process.read_stdout():
+    @classmethod
+    async def _read_stream(cls, stream: StreamReader, strip_linesep: bool = True) -> t.AsyncGenerator[str, None]:
+        async for chunk in stream:  # type: bytes
+            if strip_linesep:
+                chunk = chunk.rstrip(cls._BYTES_LINE_SEPARATOR)
+            yield chunk.decode(cls._ENCODING)
+
+    async def _read_stdout(self, process: Process) -> None:
+        if process.stdout is None:
+            self.fail("Process standard output is not available")
+        async for line in self._read_stream(process.stdout):
             self.say(line)
 
-    async def _read_stderr(self, shell_process: Shell) -> None:
-        async for line in shell_process.read_stderr():
+    async def _read_stderr(self, process: Process) -> None:
+        if process.stderr is None:
+            self.fail("Process standard output is not available")
+        async for line in self._read_stream(process.stderr):
             self.say(Stderr(line))
 
-    async def _create_shell(self) -> Shell:
+    async def _create_process(self) -> Process:
         command: str = self.args.command or f"source '{self.args.file}'"
         if C.SHELL_INJECT_YIELD_FUNCTION:
             command = f"{self._SHELL_SERVICE_FUNCTIONS_DEFINITIONS}\n{command}"
@@ -53,23 +68,43 @@ class ShellAction(EmissionScannerActionBase):
         if self.args.environment is not None:
             environment = os.environ.copy()
             environment.update(self.args.environment)
-        return Shell(
-            command=command,
-            environment=environment,
+        process = await create_subprocess_shell(
+            cmd=command,
+            stdin=PIPE,
+            stdout=PIPE,
+            stderr=PIPE,
+            env=environment,
             cwd=self.args.cwd,
             executable=self.args.executable or C.DEFAULT_SHELL_EXECUTABLE,
         )
+        return process
+
+    @contextlib.asynccontextmanager
+    async def _control_process_lifecycle(self):
+        process = await self._create_process()
+        yield process
+        if process.returncode is None:
+            process.kill()
+        # Close communication anyway
+        await process.communicate()
+        for stream in (process.stdout, process.stderr, process.stdin):
+            if stream is None:
+                continue
+            stream._transport.close()  # type: ignore[union-attr]  # pylint: disable=protected-access
+
+    async def _transmit_process_standard_streams(self, process: Process) -> None:
+        tasks: list[asyncio.Task] = [
+            asyncio.create_task(self._read_stdout(process)),
+            asyncio.create_task(self._read_stderr(process)),
+        ]
+        # Wait for all tasks to complete
+        await asyncio.wait(tasks)
+        # Check exceptions
+        await asyncio.gather(*tasks)
 
     async def run(self) -> None:
-        async with await self._create_shell() as shell_process:
-            tasks: list[asyncio.Task] = [
-                asyncio.create_task(self._read_stdout(shell_process)),
-                asyncio.create_task(self._read_stderr(shell_process)),
-            ]
-            # Wait for all tasks to complete
-            await asyncio.wait(tasks)
-            # Check exceptions
-            await asyncio.gather(*tasks)
-            result: ShellResult = await shell_process
-            if result.code:
-                self.fail(f"Exit code: {result.code}")
+        async with self._control_process_lifecycle() as process:
+            await self._transmit_process_standard_streams(process)
+            await process.communicate()
+            if process.returncode:
+                self.fail(f"Exit code: {process.returncode}")
