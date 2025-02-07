036b3a3abd9ac0651b0cb2d4906c6ca634628e6e
Artem Novikov <reartnew@nebius.com>
2025-02-07 22:38:45 +0100

    All relatable constants for RC

--- src/grana/config/constants/rc.py
+++ src/grana/config/constants/rc.py
@@ -32,8 +32,23 @@ sentinel = ConfigSentinel()
 class RC:
     """Runtime configuration"""
 
-    log_level: str | ConfigSentinel = sentinel
-    log_file: str | ConfigSentinel = sentinel
+    log_level: t.Union[str, ConfigSentinel] = sentinel
+    log_file: t.Union[str, ConfigSentinel] = sentinel
+    workflow_source_file: t.Union[pathlib.Path, ConfigSentinel] = sentinel
+    workflow_loader_source_file: t.Union[pathlib.Path, ConfigSentinel] = sentinel
+    display_source_file: t.Union[pathlib.Path, ConfigSentinel] = sentinel
+    display_name: t.Union[str, ConfigSentinel] = sentinel
+    strategy_name: t.Union[str, ConfigSentinel] = sentinel
+    use_color: t.Union[bool, ConfigSentinel] = sentinel
+    default_shell_executable: t.Union[str, ConfigSentinel] = sentinel
+    shell_inject_yield_function: t.Union[bool, ConfigSentinel] = sentinel
+    strict_outcomes_rendering: t.Union[bool, ConfigSentinel] = sentinel
+    action_classes_directories: t.Union[list[pathlib.Path], ConfigSentinel] = sentinel
+    external_python_modules_paths: t.Union[list[pathlib.Path], ConfigSentinel] = sentinel
+
+    def __post_init__(self) -> None:
+        if self.display_source_file is not sentinel and self.display_name is not sentinel:
+            raise ValueError("Mutually exclusive options `display_source_file` and `display_name` have been provided")
 
     @classmethod
     def build(cls) -> RC:
