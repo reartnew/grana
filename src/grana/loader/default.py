c965d15522bdcda6dd1e096563a2c4fa97b249a9
Artem Novikov <reartnew@nebius.com>
2025-02-07 11:29:09 +0100

    DefaultYAMLWorkflowLoader._get_static_action_factories_mapping

--- src/grana/loader/default.py
+++ src/grana/loader/default.py
@@ -51,7 +51,14 @@ class DefaultYAMLWorkflowLoader(AbstractBaseWorkflowLoader):
     ALLOWED_ROOT_TAGS: set[str] = {"actions", "context", "miscellaneous", "configuration"}
 
     def get_action_factories_mapping(self) -> dict[str, type[ActionBase]]:
-        static_action_factories: dict[str, type[ActionBase]] = {
+        return {
+            **self._get_static_action_factories_mapping(),
+            **self._load_external_action_factories_mapping(),
+        }
+
+    @lru_cache(maxsize=1)
+    def _get_static_action_factories_mapping(self) -> dict[str, type[ActionBase]]:
+        return {
             name: klass
             for name, klass in (
                 ("echo", EchoAction),
@@ -61,13 +68,9 @@ class DefaultYAMLWorkflowLoader(AbstractBaseWorkflowLoader):
             )
             if klass is not None
         }
-        return {
-            **static_action_factories,
-            **self._load_external_action_factories(),
-        }
 
     @lru_cache(maxsize=1)
-    def _load_external_action_factories(self) -> dict[str, type[ActionBase]]:
+    def _load_external_action_factories_mapping(self) -> dict[str, type[ActionBase]]:
         dynamic_bases_map: dict[str, type[ActionBase]] = {}
         for class_directory in C.ACTION_CLASSES_DIRECTORIES:  # type: str
             class_directory_path = Path(class_directory).resolve()
