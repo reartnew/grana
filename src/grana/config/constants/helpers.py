272f5053178cc92eed03fa8e0d1c7582e090b4d8
Artem Novikov <reartnew@nebius.com>
2025-02-09 13:38:52 +0100

    cyclic-import

--- src/grana/config/constants/helpers.py
+++ src/grana/config/constants/helpers.py
@@ -40,7 +40,7 @@ def add_sys_paths(*paths: Path) -> t.Iterator[None]:
 
 def load_external_module(source: Path, submodule_name: t.Optional[str] = None) -> ModuleType:
     """Load an external module"""
-    # pylint: disable=import-outside-toplevel
+    # pylint: disable=import-outside-toplevel,cyclic-import
     from ..constants import C
 
     if not source.is_file():
