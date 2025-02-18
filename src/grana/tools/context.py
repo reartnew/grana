bae5233245233e7ee777227e3eb301720fe62f7c
Artem Novikov <reartnew@nebius.com>
2025-02-18 17:55:36 +0100

    Include function to the signature

--- src/grana/tools/context.py
+++ src/grana/tools/context.py
@@ -34,38 +34,28 @@ class ContextManagerVar(t.Generic[VT]):
         return self._ctx_var.get()
 
 
-PT = t.ParamSpec("PT")
-RT = t.TypeVar("RT")
-
-
 class ContextCache:
     """Context-based cache for functions"""
 
     def __init__(self):
-        self._cache: t.Optional[ContextManagerVar[dict]] = None
+        self._cache: ContextManagerVar[dict] = ContextManagerVar()
 
     @contextlib.contextmanager
     def mount(self) -> t.Generator[None, None, None]:
         """Starts a new context for the cache"""
-        self._cache = ContextManagerVar()
-        try:
-            with self._cache.set({}):
-                yield
-        finally:
-            self._cache = None
+        with self._cache.set({}):
+            yield
 
-    def wrap(self, f: t.Callable[PT, RT]) -> t.Callable[PT, RT]:
+    def wrap(self, f: t.Callable) -> t.Callable:
         """A decorator to apply the cache to the function"""
 
         @functools.wraps(f)
-        def wrapped(*args: PT.args, **kwargs: PT.kwargs) -> RT:
-            key = functools._make_key(args, kwargs, False)  # pylint: disable=protected-access
-            if self._cache is None:
-                return f(*args, **kwargs)
+        def wrapped(*args, **kwargs):
             try:
                 local_context_cache: dict = self._cache.get()
             except LookupError:
                 return f(*args, **kwargs)
+            key = functools._make_key((f,) + args, kwargs, False)  # pylint: disable=protected-access
             if key not in local_context_cache:
                 local_context_cache[key] = f(*args, **kwargs)
             return local_context_cache[key]
