1fe94c3d61c392dfcb6bb17bfa364e3117dbff75
Artem Novikov <reartnew@nebius.com>
2025-05-18 17:18:40 +0200

    make_subflow_runner_class_for_action

--- src/grana/actions/bundled/loop.py
+++ src/grana/actions/bundled/loop.py
@@ -5,9 +5,9 @@ import dataclasses
 import itertools
 import typing as t
 
+from .subflow import make_subflow_runner_class_for_action
 from ..base import ArgsBase, ActionBase
 from ...actions import constants
-from ...display.types import DisplayEvent, DisplayEventName
 from ...exceptions import ExecutionFailed
 from ...rendering import CommonTemplar, containers as c
 
@@ -34,44 +34,18 @@ class LoopAction(ActionBase):
 
     async def run(self) -> None:
         from ...config.constants import C
-        from ...runner import Runner
 
-        action: LoopAction = self
-
-        def _resend_event_via_action(event: DisplayEvent) -> None:
-            # These events shall not pass to the parent runner
-            if event.name in (
-                DisplayEventName.ON_RUNNER_FINISH,  # Triggers final status output
-                DisplayEventName.ON_PLAN_INTERACTION,  # Pauses the execution
-            ):
-                event.future.set_result(None)  # Unlock the execution and continue
-            else:
-                self._communicator.send_display_event(event)  # Pass modified event
-
-        class LoopRunner(Runner):
-            """A runner that intercepts and filters out events"""
-
-            async def _process_display_events(self) -> None:
-                while True:
-                    event: DisplayEvent = await self._events_flow.get()
-                    _resend_event_via_action(event)
-
-            async def run_async(self) -> None:
-                try:
-                    return await super().run_async()
-                finally:
-                    for sub_action in self.workflow.values():
-                        action.yield_outcome(sub_action.name, c.OutcomeDict(sub_action.outcomes))
+        loop_runner_class = make_subflow_runner_class_for_action(self)
 
         # Cache [re]mount is required since the subflow may reconfigure some fields
         with C.mount_context_cache():
             # Check step vars
-            if unexpected_fields := set(action.args.step) & TOP_LEVEL_ONLY_ACTION_RESERVED_FIELD_NAMES:
+            if unexpected_fields := set(self.args.step) & TOP_LEVEL_ONLY_ACTION_RESERVED_FIELD_NAMES:
                 raise ValueError(f"Unexpected `step` fields: {sorted(unexpected_fields)}")
             # Prepare ranges
             var_names: list[str] = []
             all_var_values: list[t.Any] = []
-            for var_name, var_values in action.args.matrix.items():
+            for var_name, var_values in self.args.matrix.items():
                 var_names.append(var_name)
                 all_var_values.append(var_values)
             # Build actions
@@ -79,14 +53,15 @@ class LoopAction(ActionBase):
             for var_values_tuple in itertools.product(*all_var_values):
                 matrix_locals_map: dict[str, t.Any] = c.AttrDict(zip(var_names, var_values_tuple))
                 templar: CommonTemplar = self._communicator.get_templar(extra_locals=matrix_locals_map)
-                action_dict: dict = templar.render(action.args.step)
+                action_dict: dict = templar.render(self.args.step)
                 actions.append(action_dict)
+            # Prepare configuration section
             configuration: dict[str, t.Any] = {}
-            if action.args.strategy is not None:
-                configuration["strategy"] = action.args.strategy
-            if action.args.strict is not None:
-                configuration["strict"] = action.args.strict
-            runner = LoopRunner({"actions": actions, "configuration": configuration})
+            if self.args.strategy is not None:
+                configuration["strategy"] = self.args.strategy
+            if self.args.strict is not None:
+                configuration["strict"] = self.args.strict
+            runner = loop_runner_class({"actions": actions, "configuration": configuration})
             try:
                 await runner.run_async()
             except ExecutionFailed:
--- src/grana/actions/bundled/subflow.py
+++ src/grana/actions/bundled/subflow.py
@@ -12,6 +12,7 @@ from ...exceptions import ExecutionFailed
 from ...rendering.containers import OutcomeDict
 
 __all__ = [
+    "make_subflow_runner_class_for_action",
     "SubflowAction",
 ]
 
@@ -32,6 +33,55 @@ class SubflowArgsBySpec(ArgsBase):
     extra_context: dict[str, t.Any] = dataclasses.field(default_factory=dict)
 
 
+def make_subflow_runner_class_for_action(action: ActionBase) -> type:
+    """Make a specialized subflow runner for the given action."""
+    from ...runner import Runner
+
+    def _resend_event_via_action(event: DisplayEvent) -> None:
+        # These events shall not pass to the parent runner
+        if event.name in (
+            DisplayEventName.ON_RUNNER_FINISH,  # Triggers final status output
+            DisplayEventName.ON_PLAN_INTERACTION,  # Pauses the execution
+        ):
+            event.future.set_result(None)  # Unlock the execution and continue
+        else:
+            # Pass modified event
+            action._communicator.send_display_event(event)  # pylint: disable=protected-access
+
+    class SubflowRunner(Runner):
+        """A runner that intercepts and filters out events"""
+
+        async def _process_display_events(self) -> None:
+            while True:
+                event: DisplayEvent = await self._events_flow.get()
+                _resend_event_via_action(event)
+
+        @classmethod
+        def deep_update_context(cls, receiver: dict[str, t.Any], source: Mapping, path: str) -> dict[str, t.Any]:
+            """Apply changes to the context"""
+            for source_key, source_value in source.items():
+                sub_path: str = f"{path}.{source_key}" if path else source_key
+                if source_key not in receiver:
+                    cls.logger.debug(f"Adding context: {sub_path}")
+                    receiver[source_key] = source_value
+                elif isinstance(source_value, Mapping) and isinstance(receiver[source_key], MutableMapping):
+                    cls.logger.debug(f"Merging context: {sub_path}")
+                    receiver[source_key] = cls.deep_update_context(receiver[source_key], source_value, sub_path)
+                else:
+                    cls.logger.debug(f"Rewriting context: {sub_path}")
+                    receiver[source_key] = source_value
+            return receiver
+
+        async def run_async(self) -> None:
+            try:
+                return await super().run_async()
+            finally:
+                for sub_action in self.workflow.values():
+                    action.yield_outcome(sub_action.name, OutcomeDict(sub_action.outcomes))
+
+    return SubflowRunner
+
+
 class SubflowAction(ActionBase):
     """Executes an independent workflow and passes the display events to the original runner."""
 
@@ -39,68 +89,27 @@ class SubflowAction(ActionBase):
 
     async def run(self) -> None:
         from ...config.constants import C
-        from ...runner import Runner
 
-        action: SubflowAction = self
-
-        def _resend_event_via_action(event: DisplayEvent) -> None:
-            # These events shall not pass to the parent runner
-            if event.name in (
-                DisplayEventName.ON_RUNNER_FINISH,  # Triggers final status output
-                DisplayEventName.ON_PLAN_INTERACTION,  # Pauses the execution
-            ):
-                event.future.set_result(None)  # Unlock the execution and continue
-            else:
-                self._communicator.send_display_event(event)  # Pass modified event
-
-        class SubflowRunner(Runner):
-            """A runner that intercepts and filters out events"""
-
-            async def _process_display_events(self) -> None:
-                while True:
-                    event: DisplayEvent = await self._events_flow.get()
-                    _resend_event_via_action(event)
-
-            @classmethod
-            def _deep_update_context(cls, receiver: dict[str, t.Any], source: Mapping, path: str) -> dict[str, t.Any]:
-                """Apply changes to the context"""
-                for source_key, source_value in source.items():
-                    sub_path: str = f"{path}.{source_key}" if path else source_key
-                    if source_key not in receiver:
-                        cls.logger.debug(f"Adding context: {sub_path}")
-                        receiver[source_key] = source_value
-                    elif isinstance(source_value, Mapping) and isinstance(receiver[source_key], MutableMapping):
-                        cls.logger.debug(f"Merging context: {sub_path}")
-                        receiver[source_key] = cls._deep_update_context(receiver[source_key], source_value, sub_path)
-                    else:
-                        cls.logger.debug(f"Rewriting context: {sub_path}")
-                        receiver[source_key] = source_value
-                return receiver
-
-            async def run_async(self) -> None:
-                self.workflow.context = self._deep_update_context(
-                    receiver=self.workflow.context,
-                    source=action.args.extra_context,
-                    path="",
-                )
-                try:
-                    return await super().run_async()
-                finally:
-                    for sub_action in self.workflow.values():
-                        action.yield_outcome(sub_action.name, OutcomeDict(sub_action.outcomes))
+        loop_runner_class = make_subflow_runner_class_for_action(self)
 
         # Cache [re]mount is required since the subflow may reconfigure some fields
         with C.mount_context_cache():
             source: t.Union[Path, dict]
-            if isinstance(action.args, SubflowArgsByPath):
-                source = action.args.path
+            if isinstance(self.args, SubflowArgsByPath):
+                source = self.args.path
             else:
                 source = {
-                    "actions": action.args.actions,
-                    "context": action.args.context,
-                    "configuration": action.args.configuration,
+                    "actions": self.args.actions,
+                    "context": self.args.context,
+                    "configuration": self.args.configuration,
                 }
-            runner = SubflowRunner(source)
+            runner = loop_runner_class(source)
+            # Update context with extras
+            runner.workflow.context = runner.deep_update_context(
+                receiver=runner.workflow.context,
+                source=self.args.extra_context,
+                path="",
+            )
             try:
                 await runner.run_async()
             except ExecutionFailed:
